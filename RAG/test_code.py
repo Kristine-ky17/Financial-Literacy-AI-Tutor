e7a92ca99b8388fd41883a8e7384eb9511879233
mariooohzc <mariohanzel@outlook.com>
2025-04-03 01:32:51 +0800

    created function to read pdfs

---
+++ RAG/test_code.py
@@ -0,0 +1,5 @@
+from utils import *
+
+
+texts = load_pdfs_from_folder("data")
+print(texts)
--- RAG/utils.py
+++ RAG/utils.py
@@ -1,1 +1,15 @@
 # Helper functions will be stored in this file
+import os
+import PyPDF2
+
+def load_pdfs_from_folder(folder_path):
+    texts = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".pdf"):
+            with open(os.path.join(folder_path, filename), "rb") as f:
+                reader = PyPDF2.PdfReader(f)
+                text = ""
+                for page in reader.pages:
+                    text += page.extract_text() or ""
+                texts.append(text)
+    return texts
