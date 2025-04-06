fefecdfd8a62c3b829361e4300b71e30713344f7
mariooohzc <mariohanzel@outlook.com>
2025-04-06 17:13:17 +0800

    updated poc, response would now return the sources

--- RAG/utils.py
+++ RAG/utils.py
@@ -3,16 +3,26 @@ import os
 import PyPDF2
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-def load_pdfs_from_folder(folder_path):
+# def load_pdfs_from_folder(folder_path):
+#     texts = []
+#     for filename in os.listdir(folder_path):
+#         if filename.endswith(".pdf"):
+#             with open(os.path.join(folder_path, filename), "rb") as f:
+#                 reader = PyPDF2.PdfReader(f)
+#                 text = ""
+#                 for page in reader.pages:
+#                     text += page.extract_text() or ""
+#                 texts.append(text)
+#     return texts
+
+def load_text_from_pdf(filename):
     texts = []
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".pdf"):
-            with open(os.path.join(folder_path, filename), "rb") as f:
-                reader = PyPDF2.PdfReader(f)
-                text = ""
-                for page in reader.pages:
-                    text += page.extract_text() or ""
-                texts.append(text)
+    with open(filename, "rb") as f:
+        reader = PyPDF2.PdfReader(f)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text() or ""
+        texts.append(text)
     return texts
 
 def chunk_texts(texts, chunk_size=500, overlap=50):
