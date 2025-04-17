a6e7b481de7f9a811e193bdb1a3a1625538c26a7
mariooohzc <mariohanzel@outlook.com>
2025-04-17 13:58:15 +0800

    changed retrieval_qa to lcel

--- RAG/app.py
+++ RAG/app.py
@@ -4,9 +4,13 @@ import os
 from langchain_pinecone import PineconeVectorStore
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain.prompts import ChatPromptTemplate
+# from langchain.chains import RetrievalQA
 import json
 from pinecone import Pinecone
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough, RunnableParallel
 
 load_dotenv()
 
@@ -42,37 +46,67 @@ def answer_qn(namespace: str, question: str):
         index=index, embedding=OpenAIEmbeddings(), namespace=namespace
     )
 
+    retriever = vectorstore.as_retriever(search_kwargs = {"k":3})
+    prompt_str = (
+        "Answer the question below using the context:\n"
+        "Context: {context}\n"
+        "Question: {question}\n"
+        "Answer:"
+    )
+    prompt = ChatPromptTemplate.from_template(prompt_str)
 
-    llm = ChatOpenAI(
-        model_name="gpt-4o", temperature=0, openai_api_key=os.getenv("OPENAI_API_KEY")
+    chain = RunnableParallel({
+        "context":  retriever,
+        "question": RunnablePassthrough(),
+    }) | prompt | ChatOpenAI(
+        model_name = "gpt-4o",
+        temperature = 0,
+        openai_api_key=os.getenv("OPENAI_API_KEY")
     )
 
-    qa_chain = RetrievalQA.from_chain_type(
-        llm=llm,
-        retriever=vectorstore.as_retriever(search_kwargs={"k": 3}),
-        return_source_documents=True,
-    )
+    answer = chain.invoke(question).content
 
-    response = qa_chain({"query": question})
+    st.subheader("Answer:")
+    st.write(answer)
 
-    if user_question:
-        with st.spinner("Searching for the answer..."):
-            response = qa_chain({"query": user_question})
-            st.subheader("Answer:")
-            st.write(response["result"])
+    docs = retriever.invoke(user_question)
+    docs = set([doc.metadata.get("source") for doc in docs])
 
-            st.subheader("Sources used:")
-            for doc in response["source_documents"]:
-                st.write(f"- {metadata_lookup.get(doc.metadata['source'], doc.metadata['source'])}")
+    st.subheader("Sources used:")
+    for source_key in docs:
+        # source_key = doc.metadata.get("source")
+        st.write(f"- {metadata_lookup.get(source_key, source_key)}")
+
+    # llm = ChatOpenAI(
+    #     model_name="gpt-4o", temperature=0, openai_api_key=os.getenv("OPENAI_API_KEY")
+    # )
+
+    # qa_chain = RetrievalQA.from_chain_type(
+    #     llm=llm,
+    #     retriever=vectorstore.as_retriever(search_kwargs={"k": 3}),
+    #     return_source_documents=True,
+    # )
+
+    # response = qa_chain({"query": question})
+
+    # if user_question:
+    #     with st.spinner("Searching for the answer..."):
+    #         response = qa_chain({"query": user_question})
+    #         st.subheader("Answer:")
+    #         st.write(response["result"])
+
+    #         st.subheader("Sources used:")
+    #         for doc in response["source_documents"]:
+    #             st.write(f"- {metadata_lookup.get(doc.metadata['source'], doc.metadata['source'])}")
 
 
-    print("Answer:")
-    print(response["result"])
+    # print("Answer:")
+    # print(response["result"])
 
-    print("\n Sources used:")
-    for doc in response["source_documents"]:
-        # print(f"- {doc.metadata['source']}")
-        print(f"- {metadata_lookup.get(doc.metadata['source'])}")
+    # print("\n Sources used:")
+    # for doc in response["source_documents"]:
+    #     # print(f"- {doc.metadata['source']}")
+    #     print(f"- {metadata_lookup.get(doc.metadata['source'])}")
 
 
 st.title("Finance Document Q&A")
